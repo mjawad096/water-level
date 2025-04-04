d7dfca53434764f836e04740d71c51e77317ed2c
Muhammad Jawad <muhammad.jawad@digitaltolk.com>
2025-04-04 20:06:03 +0500

    improvements

--- src/main.ino
+++ src/main.ino
@@ -40,32 +40,30 @@ void loop()
     if (espNow.isLastUpdatedMoreThan(3))
     {
         led.blinkFor(100);
-        // buzzer.start(1, 60000); // Continue beeping for 1 minute
+    }
+    else if (waterLevelData->isPumpOn)
+    {
+        led.on(true);
     }
     else
     {
         led.blinkFor(1500);
-
-        if (isLowLevel())
-        {
-            buzzer.start(2, 60000, 400); // Beep for 1 minute with 400ms cycle
-        }
-        else if (isHighLevel())
-        {
-            buzzer.start(3, 60000, 200); // Beep for 1 minute with 800ms cycle
-        }
-        else
-        {
-            buzzer.stop(true);
-
-            if (waterLevelData->isPumpOn)
-            {
-                led.on(true);
-            }
-        }
-
-        display.displayLevel(waterLevelData->level);
     }
+
+    if (isLowLevel())
+    {
+        buzzer.start(2, 60000, 400);
+    }
+    else if (isHighLevel())
+    {
+        buzzer.start(3, 60000, 200);
+    }
+    else
+    {
+        buzzer.stop(true);
+    }
+
+    display.displayLevel(waterLevelData->level);
 }
 
 bool isLowLevel()
